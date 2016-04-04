7806dc78666c4998b841017e1a08fad025e3474c
junqing.fjq <junqing.fjq@alibaba-inc.com>
2016-04-05 01:06:02 +0800

    update

---
+++ goforit-common-service/lock-service-core/src/main/java/com/goforit/common/service/lock/mapper/DAO/SequenceDO.java
@@ -0,0 +1,7 @@
+package com.goforit.common.service.lock.mapper.DAO;
+
+/**
+ * Created by junqingfjq on 16/4/5.
+ */
+public class SequenceDO {
+}
---
+++ goforit-common-service/lock-service-core/src/main/java/com/goforit/common/service/lock/mapper/SequenceMapper.java
@@ -0,0 +1,7 @@
+package com.goforit.common.service.lock.mapper;
+
+/**
+ * Created by junqingfjq on 16/4/5.
+ */
+public interface SequenceMapper {
+}
